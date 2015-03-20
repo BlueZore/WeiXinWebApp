a64941d8fdf452d354628900ce7642539e0d7db7
徐松涛 <kally32@163.com>
2015-03-20 11:11:21 +0800

    1

---
+++ WebApp/WebApp/WebService.asmx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Services;
+
+namespace WebApp
+{
+    /// <summary>
+    /// WebService 的摘要说明
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+
+    public class WebService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+    }
+}
